6bbe41e4e93ffebef268b3fddb122ba94b9a9bf0
Tawaccul <teps2222@gmail.com>
2021-11-28 23:20:04 +0300

    covid

---
+++ 1.js
@@ -0,0 +1,47 @@
+require('dotenv').config();
+
+const { Telegraf } = require('telegraf');
+
+const api = require('covid19-api');
+
+const COUNTRIES_HELP = require('./const');
+
+const {Markup}  = require('telegraf');
+
+const bot = new Telegraf(process.env.Bot_token);
+
+bot.start((ctx) => ctx.reply(`Привет, ${ctx.message.from.first_name}!
+Узнай статистику по Коронавирусу. 
+Для этого напиши на английском название любой страны. 
+`,
+
+Markup.keyboard([
+['US', 'Russia', 'Qatar', 'Kazakhstan'],
+['Ukraine', 'Japan', 'Poland', 'Belarus'],
+	]).resize() 
+  )
+);
+bot.help((ctx) => ctx.reply(COUNTRIES_HELP));
+
+bot.on( 'text', async (ctx) => {
+	let data = {};
+	try{
+	data = await api.getReportsByCountries(ctx.message.text);
+
+	const formatData = `
+Страна: ${data[0][0].country}
+Заболело: ${data[0][0].cases}
+Умерло: ${data[0][0].deaths}
+Вылечились: ${data[0][0].recovered}  
+	`;
+	ctx.reply(formatData);
+}catch {
+	ctx.reply(`Введите правильное название страны. 
+Список названий: /help`);
+}
+
+}); 
+
+bot.hears('hi', (ctx) => ctx.reply('Hey there'));
+bot.launch();
+
